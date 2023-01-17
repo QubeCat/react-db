d2d0fac47d7646f7c60d83fa8598414d78a52d8b
alif <alifrazanit@gmail.com>
2023-01-17 09:31:38 +0700

    177

--- src/App.js
+++ src/App.js
@@ -1,40 +1,38 @@
-import React, { useState } from 'react';
+import React, { useState } from "react";
 
-import MoviesList from './components/MoviesList';
-import './App.css';
+import MoviesList from "./components/MoviesList";
+import "./App.css";
 
 function App() {
-  const [movie, setMovie ] = useState([]);
+  const [movie, setMovie] = useState([]);
 
-  function fetchMovieHandler () {
-    fetch('https://swapi.dev/api/films/').then(response => {
-      return response.json()
-    }).then(data => {
-      const transformData = data.results.map(movieData => {
-        return {
-          id: movieData.episode_id,
-          title: movieData.title,
-          openingText: movieData.opening_crawl,
-          releaseDate: movieData.release_date
-        }
-      })
-
-      setMovie(transformData);
-    })
+  async function fetchMovieHandler() {
+    const response = await fetch("https://swapi.dev/api/films/");
+    const data = await response.json();
+    console.log(data)
+    const transFormData = data.results.map((movieData) => {
+      return {
+        id: movieData.episode_id,
+        title: movieData.title,
+        openingText: movieData.opening_crawl,
+        releaseDate: movieData.release_date,
+      };
+    });
+    setMovie(transFormData);
   }
 
   const dummyMovies = [
     {
       id: 1,
-      title: 'Some Dummy Movie',
-      openingText: 'This is the opening text of the movie',
-      releaseDate: '2021-05-18',
+      title: "Some Dummy Movie",
+      openingText: "This is the opening text of the movie",
+      releaseDate: "2021-05-18",
     },
     {
       id: 2,
-      title: 'Some Dummy Movie 2',
-      openingText: 'This is the second opening text of the movie',
-      releaseDate: '2021-05-19',
+      title: "Some Dummy Movie 2",
+      openingText: "This is the second opening text of the movie",
+      releaseDate: "2021-05-19",
     },
   ];
 
